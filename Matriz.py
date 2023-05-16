98e1fbaecc4a938d66c2050ecccd10ee8e862ebd
Juan Andres Salazar Toro <juan.salazar222@tau.usbmed.edu.co>
2023-05-16 01:47:26 -0500

    V1

---
+++ Matriz.py
@@ -0,0 +1,15 @@
+import math
+import random
+
+class Matriz():
+
+    def __init__(self) -> None:
+        pass
+    
+    def ranvec(self, l= 1) -> list:
+        import random as m
+        v = [random.randint(-100,100) for vi in range(l)]
+        return v
+    
+    def request_vec(self) -> int:
+        
