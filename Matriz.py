b94a65aa69a174df9420534180f23277058acac0
Juan Andres Salazar Toro <133258856+andresst0345@users.noreply.github.com>
2023-05-18 00:28:34 -0500

    v2

--- Matriz.py
+++ Matriz.py
@@ -6,10 +6,13 @@ class Matriz():
     def __init__(self) -> None:
         pass
     
-    def ranvec(self, l= 1) -> list:
+    def ranvec(self, f= 1, c= 1) -> list:
         import random as m
-        v = [random.randint(-100,100) for vi in range(l)]
+        v = [random.randint(-100,100) for vi in range(f)]
         return v
     
-    def request_vec(self) -> int:
-        pass
+    def sumvec(self ,v1 ,v2 ) -> list:
+        suma = []
+        for i in range(len(v1)):
+            suma.append(v1[i] + v2[i])
+        return suma
