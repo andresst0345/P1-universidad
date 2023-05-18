08ac36cfaff59380f38aa2cd653009675660d625
Juan Andres Salazar Toro <juan.salazar222@tau.usbmed.edu.co>
2023-05-18 13:20:42 -0500

    Ck

--- Matriz.py
+++ Matriz.py
@@ -1,15 +1,16 @@
-import math
 import random
 
 class Matriz():
+    import math
+    
 
     def __init__(self,f ,c ,v1 ,v2 ) -> None:
-        self.f = filas
-        self.c = columnas
-        self.v1 = vector1
+        self.f = f
+        self.c = c
+        self.v1 = v1
+        self.v2 = v2
 
         
-    
     def ranvec(self, f= 1, c= 1) -> list:
         import random as m
         mtz = []
@@ -18,7 +19,7 @@ class Matriz():
          mtz.append(fil)
         return mtz
     
-    def sumvec(self ,m1 ,m2 ) -> list:
+    def summt(self ,m1 ,m2 ) -> list:
         if len(m1) != len(m2):
             raise ValueError("Las matrices deben tener el mismo tamaño")
         suma = []
@@ -28,6 +29,17 @@ class Matriz():
               fil.append(m1[ii][j] + m2[ii][j])
            suma.append(fil)
 
+    def restmt(self, m1, m2):
+        if len(m1) != len(m2):
+            raise ValueError("Las matrices deben tener el mismo tamaño")
+        rest = []
+        for ii in range(len(m1)):
+           fil = []
+           for j in range(len(m1)):
+              fil.append(m1[ii][j] - m2[ii][j])
+           rest.append(fil)
+        return rest
+
     def multvec(self,m1 ,m2 ):
         mult = []
         if len(m1[0]) != len(m2):
@@ -36,8 +48,16 @@ class Matriz():
         col_m1 = len(m1[0])
         col_m2 = len(m2[0])
 
-        return mult
+        
 
+        return mult
+    
+    def divmt(self, m1, m2):
+
+
+
+
+        return pass
 #test func
 
 v = ranvec(4, 4)
