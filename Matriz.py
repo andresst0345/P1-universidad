551ebf4bf735a01e43215d12cb38e7950c5c816f
Juan Andres Salazar Toro <juan.salazar222@tau.usbmed.edu.co>
2023-05-18 03:52:03 -0500

    v3

--- Matriz.py
+++ Matriz.py
@@ -3,16 +3,29 @@ import random
 
 class Matriz():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,f ,c ,v1 ,v2 ) -> None:
+        self.f = filas
+        self.c = columnas
+        self.v1 = vector1 
+
+        
     
     def ranvec(self, f= 1, c= 1) -> list:
         import random as m
-        v = [random.randint(-100,100) for vi in range(f)]
-        return v
+        mtz = []
+        for i in range(c):
+         fil = [random.randint(-100,100) for vi in range(f)]
+         mtz.append(fil)
+        return mtz
     
     def sumvec(self ,v1 ,v2 ) -> list:
         suma = []
         for i in range(len(v1)):
             suma.append(v1[i] + v2[i])
         return suma
+    
+
+#test func
+
+v = randvec(4, 4)
+
